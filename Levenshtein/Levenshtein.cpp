c1b86b20d1bf5925b503b50f74c67ebe39c50371
Joey Soroka <90403963+Joey574@users.noreply.github.com>
2023-09-28 11:24:32 -0700

    started getting threads working, something weird going on

--- Levenshtein/Levenshtein.cpp
+++ Levenshtein/Levenshtein.cpp
@@ -1,10 +1,42 @@
 #include <iostream>
+#include <thread>
+#include <conio.h>
 
 using namespace std;
 
+string firstWord;
+string secondWord;
+
+int MAX_THREADS = 16;
+
+class mapThread {
+private:
+    int threadID;
+    string threadName;
+
+
+    mapThread(int threadID) {
+        this->threadID = threadID;
+        threadName = "\nT-" + threadID;
+        cout << threadName + " running";
+    }
+};
+
 int main()
 {
-    cout << "This is the basic Levenshtein in Cpp\n";
+    cout << "This is the basic multi-threaded Levenshtein in Cpp\n";
+    _getch();
+    system("CLS");
+    cout << "Enter the first word\nInput: ";
+    cin >> firstWord;
+    system("CLS");
+    cout << "Enter the second word\nInput: ";
+    cin >> secondWord;
+
+    for (int i = 0; i < MAX_THREADS; i++) {
+        cout << "Thread " + i + " created";
+        thread thread(mapThread(i));
+    }
 
 
 }

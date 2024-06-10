fa993bf7eebf242e7ecd4b9db70fa66533d46342
Joey Soroka <90403963+Joey574@users.noreply.github.com>
2024-06-10 14:35:07 -0700

    more work on the snake game, forgot how weird it was to make this in unity

--- Snake_Game/Snake_Game.cpp
+++ Snake_Game/Snake_Game.cpp
@@ -3,39 +3,47 @@
 #include <random>
 #include <Windows.h>
 
-void draw_board(std::vector<std::vector<uint8_t>> game_board);
-std::vector <std::vector<uint8_t>> generate_food(std::vector<std::vector<uint8_t>> game_board);
-std::vector<std::vector<uint8_t>> move_snake(std::vector<std::vector < uint8_t>> game_board, uint8_t direction);
-
-/*
-Game State
-0 -> empty space
-1 -> snake body
-2 -> food
-3 -> snake head
-*/
-
+struct pos {
+	int x;
+	int y;
+};
+enum tile_type {
+	none, food, body, head
+};
 enum direction {
 	up, down, left, right
 };
+
+void draw_board(std::vector<std::vector<tile_type>> game_board);
+std::vector <std::vector<tile_type>> generate_food(std::vector<std::vector<tile_type>> game_board);
+std::vector<std::vector<tile_type>> move_snake(std::vector<std::vector < tile_type>> game_board, direction dir);
+
+std::vector<pos> snake;
 
 int main()
 {
 	int width = 20;
 	int height = 20;
 
-	std::vector<std::vector<uint8_t>> game_board;
+	std::vector<std::vector<tile_type>> game_board;
 
 	for (int i = 0; i < height; i++) {
-		game_board.emplace_back(std::vector<uint8_t>(width, 0));
+		game_board.emplace_back(std::vector<tile_type>(width, tile_type::none));
 	}
 
-	game_board[13][10] = 3;
+	snake.emplace_back(height / 2, width / 2);
+	snake.emplace_back(height / 2, width / 2 - 1);
+	snake.emplace_back(height / 2, width / 2 - 2);
 
-	move_snake(game_board, 1);
+	draw_board(game_board);
+	game_board = move_snake(game_board, direction::up);
+
+	std::cout << "\n\n";
+
+	draw_board(game_board);
 }
 
-std::vector<std::vector<uint8_t>> generate_food(std::vector<std::vector<uint8_t>> game_board) {
+std::vector<std::vector<tile_type>> generate_food(std::vector<std::vector<tile_type>> game_board) {
 	
 	int min = 0;
 	int max = game_board.size() - 1;
@@ -49,7 +57,7 @@ std::vector<std::vector<uint8_t>> generate_food(std::vector<std::vector<uint8_t>
 		int rand_y = distrib(gen);
 
 		if (game_board[rand_x][rand_y] == 0) {
-			game_board[rand_x][rand_y] = 2;
+			game_board[rand_x][rand_y] = tile_type::food;
 			break;
 		}
 	}
@@ -57,45 +65,38 @@ std::vector<std::vector<uint8_t>> generate_food(std::vector<std::vector<uint8_t>
 	return game_board;
 }
 
-std::vector<std::vector<uint8_t>> move_snake(std::vector<std::vector < uint8_t>> game_board, direction dir) {
+std::vector<std::vector<tile_type>> move_snake(std::vector<std::vector<tile_type>> game_board, direction dir) {
 
-	int y = 0;
-	int x = 0;
-	int index = 0;
-
-	// Find position of snake head
-	while (game_board[x][y] != 3) {
-		index++;
-
-		x = index % game_board.size();
-		y = index / game_board.size();
-	}
-	std::cout << x << " :: " << y;
+	game_board[snake[0].y][snake[0].x] = tile_type::body;
 
 	if (dir == direction::up) {
-
+		game_board[snake[0].y - 1][snake[0].x] = tile_type::head;
+		snake[0].y--;
 	} else if (dir == direction::down) {
-
+		game_board[snake[0].y + 1][snake[0].x] = tile_type::head;
+		snake[0].y++;
 	} else if (dir == direction::left) {
-
+		game_board[snake[0].y][snake[0].x - 1] = tile_type::head;
+		snake[0].x--;
 	} else if (dir == direction::right) {
-
+		game_board[snake[0].y][snake[0].x + 1] = tile_type::head;
+		snake[0].x++;
 	}
 
 	return game_board;
 }
 
-void draw_board(std::vector<std::vector<uint8_t>> game_board) {
+void draw_board(std::vector<std::vector<tile_type>> game_board) {
 
 	for (int i = 0; i < game_board.size(); i++) {
 		for (int x = 0; x < game_board[i].size(); x++) {
-			if (game_board[i][x] == 0) {
+			if (game_board[i][x] == tile_type::none) {
 				std::cout << ". ";
-			} else if (game_board[i][x] == 1) {
+			} else if (game_board[i][x] == tile_type::body) {
 				std::cout << "O ";
-			} else if (game_board[i][x] == 2) {
+			} else if (game_board[i][x] == tile_type::food) {
 				std::cout << "X ";
-			} else if (game_board[i][x] == 3) {
+			} else if (game_board[i][x] == tile_type::head) {
 				std::cout << "@ ";
 			}
 		}
